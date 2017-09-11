e0a52f304b296d4bf5fcc42fda446ad174416bbc
Chris <otienoshikuku@gmail.com>
2017-09-12 00:33:38 +0300

    Newest commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2.7
 def main(msg):
 
-    #!Adding another comment to this file.
+    # Another comment.
+    # Adding another comment to this file.
     # Print a message.
     print(msg)
 main("Hello people")
